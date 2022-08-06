2cef7aef5f4bd72b466b2b6d4617befdf9795081
Daniel Ramos <devdsfr@gmail.com>
2022-08-06 10:12:36 -0300

    feature/create pattener index

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var app = express();
+
+//logar toda as requisições que foram feitas 
+app.use(morgan('dev'));
+
+// Utilizado para fazer a divisão das requisições do postman
+app.use(bodyParser.urlencoded({extended: false}));
+//Pegar tudo que é json no post e put
+app.use(bodyParser.json());
+
+app.get('/', function(requisicao, resposta, proximo){
+    resposta.send('ok');
+})
+
+//validando a porta que esta sendo usada
+app.listen(3000, function(){
+    console.log('rodando na porta 3000');
+});
+
+module.exports = app;
