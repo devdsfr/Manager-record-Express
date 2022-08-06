c32f528299c2978f018b53f104c1a27e24c26e37
Daniel <devdsfr@gmail.com>
2022-08-06 18:22:01 -0300

    feature/adjust routes

--- routes/rt_recorde.js
+++ routes/rt_recorde.js
@@ -23,9 +23,43 @@ routes.get('/id/:identificacao', function(req, res){
     }
     Recorde.findById(req.params.identificacao).select('_id username recorde').exec(function(err,recorde){
         if(err) return res.status(403).send({error: err});
-        if(!recorde) return res.status(403).send({error: "Recorde dont exist id" });
+        if(!recorde) return res.status(403).send({error: "Record does not exist for this id" });
         return res.send({recorde});
     });
 })
+
+//GET pegar um recorde pelo token
+routes.get('/token/:token', function(req, res){
+    if(!req.params.token){
+        return res.status(422).send({error: "No data parameters"});
+    }
+    Recorde.findOne({
+        token: req.params.token
+    }).select('_id username recorde').exec(function(err,recorde){  
+        if(err) return res.status(403).send({error: err});
+        if(!recorde) return res.status(403).send({error: "Record does not exist for this token"});
+        return res.send({recorde});
+    });
+})
+
+//PUT atualizar um recorde pelo token
+routes.put('/', function(req, res){
+    if(!req.body.token || !req.body.recorde){
+        return res.status(422).send({error: "No data parameters"}); 
+    }
+    Recorde.findOne({
+        token: req.body.token
+    }).exec(function(err, recorde){
+        if(err) return res.status(403).send({error: err});
+        if(!recorde) return res.status(403).send({error: "Record does not exist for this token"});
+        if(parseFloat(req.body.recorde) > recorde.recorde){
+            recorde.recorde = parseFloat(req.body.recorde);
+        }
+        recorde.save(function(err){
+            if(err) return res.status(403).send({error: err});
+            return res.send({recorde});
+        });
+    });
+});
 
 module.exports = routes;
